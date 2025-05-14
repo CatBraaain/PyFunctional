ab8695b64ee6e35df6b1f2d7193b629e207131b3
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-05-14 22:34:27 +0900

    fix: fix type hint of .group_by()

--- functional/pipeline.py
+++ functional/pipeline.py
@@ -1081,7 +1081,7 @@ class Sequence(Generic[_T_co]):
 
     def group_by(
         self, func: Callable[[_T_co], _T]
-    ) -> Sequence[tuple[_T, Sequence[_T_co]]]:
+    ) -> Sequence[tuple[_T, list[_T_co]]]:
         """
         Group elements into a list of (Key, Value) tuples where func creates the key and maps
         to values matching that key.
